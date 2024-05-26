e00c83c41d3e990c1b77406ebd7e872f8f8d0c38
Rania Siddiqui <rs07494@st.habib.edu.pk>
2024-05-26 19:14:52 +0500

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,47 @@
+Color_grid = "#7FFFD4"
+Color_EmptyCell = "#FFEFDB"
+Font_ScoreLabel = ("Times", 24)
+Font_Score = ("Times", 48, "bold")
+Font_GameOver = ("Times", 48, "bold")
+Font_Color_GameOver = "#FFD39B"
+Winner_BG = "#ffcc00"
+Loser_BG = "#a39489"
+Color_Cells = {
+2: "#fcefe6",
+4: "#f2e8cb",
+8: "#f5b682",
+16: "#f29446",
+32: "#ff775c",
+64: "#e64c2e",
+128: "#ede291",
+256: "#fce130",
+512: "#ffdb4a",
+1024: "#f0b922",
+2048: "#fad74d"
+}
+Color_CellNumber = {
+2: "#695c57",
+4: "#695c57",
+8: "#FFFAFA",
+16: "#FFFAFA",
+32: "#FFFAFA",
+64: "#FFFAFA",
+128: "#FFFAFA",
+256: "#FFFAFA",
+512: "#FFFAFA",
+1024: "#FFFAFA",
+2048: "#FFFAFA"
+}
+Fonts_CellNumebr = {
+2: ("Times", 55, "bold"),
+4: ("Times", 55, "bold"),
+8: ("Times", 55, "bold"),
+16: ("Times", 50, "bold"),
+32: ("Times", 50, "bold"),
+64: ("Times", 50, "bold"),
+128: ("Times", 45, "bold"),
+256: ("Times", 45, "bold"),
+512: ("Times", 45, "bold"),
+1024: ("Times", 40, "bold"),
+2048: ("Times", 40, "bold"),
+}
